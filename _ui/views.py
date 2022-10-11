5e1be8edca630a2fcf8a4d4d98b12c955c1adfaa
Davide Wiest <davide.wiest2@gmail.com>
2022-10-11 18:55:08 +0200

    add form example in view

--- _ui/views.py
+++ _ui/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
+import traceback
+
 from .base import build_params, allowed_languages, choose_lang, handle_userdata, populate_form
 from .forms import signupForm
 
@@ -10,8 +13,27 @@ def main(request):
     
     request.session = handle_userdata(request)
 
-    form = populate_form(signupForm())
     params = {"title": "PROJECT_NAME"}
+
+    if request.method == "POST":
+        form = populate_form(signupForm(request.POST), l)
+        if form.is_valid():
+            clfm = form.cleaned_data
+
+
+
+
+            try:
+                pass
+
+
+            
+            except Exception as e:
+                print(traceback.format_exc(e))
+                messages.add_message(request, messages.ERROR, f"Account could not be created. Something went wrong.")
+                form.initial=clfm
+    else:
+        form = populate_form(signupForm(), l)
 
     params["signupForm"] = form
     return render(request, "main.html", build_params(["main"], params, l))
