1297bb400f859f853e9d64552320296db70ddfa4
Davide Wiest <davide.wiest2@gmail.com>
2022-10-16 21:46:29 +0200

    fix api_view() mistake

--- _api/urls.py
+++ _api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.status)
+    path("status", views.status)
 ]
--- _api/views.py
+++ _api/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .apihelper import successful_authentication, AUTH_ERR
 
-@api_view
+@api_view()
 def status(request):
     if not successful_authentication(request):
         return Response(AUTH_ERR)
