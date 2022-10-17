b3696678eb0af6536f4364d21676f43f585da64f
Davide Wiest <davide.wiest2@gmail.com>
2022-10-18 00:40:49 +0200

    debugging 3

--- PROJECT_NAME/settings.py
+++ PROJECT_NAME/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "_base_static"),
 )
 
 # Default primary key field type
