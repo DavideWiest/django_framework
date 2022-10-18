9d7646d8afa61bfbf1fab45ae70c73e1cf5957eb
Davide Wiest <davide.wiest2@gmail.com>
2022-10-18 22:15:17 +0200

    make _base_static a valid static url

--- PROJECT_NAME/settings.py
+++ PROJECT_NAME/settings.py
@@ -154,11 +154,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "_base_static/" # 'static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "_base_static"),
-)
+# STATICFILES_DIRS = [
+#     BASE_DIR / "_base_static"
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
