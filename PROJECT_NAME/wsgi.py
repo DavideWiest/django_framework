2cec43c3dc37758adf207b9ee19c32554109d643
Davide R. Wiest <davide.wiest2@gmail.com>
2022-12-25 00:12:22 +0100

    Update wsgi.py

--- PROJECT_NAME/wsgi.py
+++ PROJECT_NAME/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
